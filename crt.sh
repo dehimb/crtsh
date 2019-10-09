e771d328b16afb4c7025678ccd8d9734f6ebbfa6
dehimb <dehimb@gmail.com>
2019-10-09 16:50:45 +0300

    Append new uniq results to output file

--- crt.sh
+++ crt.sh
@@ -5,6 +5,7 @@
 RESULTS_PATH=`pwd`/crtsh
 mkdir -p $RESULTS_PATH 
 rm -rf $RESULTS_PATH/*
+OUTPUT_FILENAME=domains_crt.txt
 total=1
 count=1
 
@@ -23,5 +24,6 @@ if [ -f "$1" ]; then
 else
 	request_subdomains $1
 fi
-cat $RESULTS_PATH/responsive_* | sed -E 's/http\:\/\/|https\:\/\///g' | sort -u -o $RESULTS_PATH/domains_crt.txt
-cp $RESULTS_PATH/domains_crt.txt `pwd`/domains_crt.txt
+cat $RESULTS_PATH/responsive_* | sed -E 's/http\:\/\/|https\:\/\///g' | sort -u -o $RESULTS_PATH/$OUTPUT_FILENAME
+cat $RESULTS_PATH/domains_crt.txt >> `pwd`/$OUTPUT_FILENAME
+sort -u -o `pwd`/$OUTPUT_FILENAME `pwd`/$OUTPUT_FILENAME
